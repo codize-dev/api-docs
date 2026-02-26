86f886e296c84f236529a0a6e13539a977c7af84
koki-develop <kou.pg.0131@gmail.com>
2026-02-27 00:34:44 +0900

    feat: Implement website

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,5 +1,13 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-  hello world
-  </div>
-`;
+import { createApiReference } from "@scalar/api-reference";
+import "@scalar/api-reference/style.css";
+import openapi from "./openapi.json";
+
+createApiReference("#app", {
+  content: openapi,
+  agent: {
+    disabled: true,
+  },
+  showDeveloperTools: "never",
+  hideClientButton: true,
+  hideTestRequestButton: true,
+});
